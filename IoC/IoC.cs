aba5410c4cc001d0e2b3b7a750073cb2d2e9d3c6
Juan Ignacio Lotito <jlotito@nosis.com>
2024-05-02 12:04:59 -0300

    feature: add dependency injection

---
+++ IoC/IoC.cs
@@ -0,0 +1,22 @@
+﻿using PDFService.API.Repositories;
+using PDFService.API.Repositories.Interface;
+using PDFService.API.Services;
+using PDFService.API.Services.Interface;
+
+namespace PDFService.API.IoC
+{
+    public static class IoC
+    {
+        public static IServiceCollection AddDependency(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            // Services
+            services.AddScoped<IPdfService, PdfService>();
+
+            // General 
+            services.AddScoped<IHttpClienteRepositories, HttpClienteRepositories>();
+
+            return services;
+        }
+    }
+}
