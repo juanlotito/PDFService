5da56803f818f49250abea4df54332790c4b1fcd
Juan Ignacio Lotito <jlotito@nosis.com>
2024-05-02 12:05:35 -0300

    feature: add generic Response model class

---
+++ Models/Response.cs
@@ -0,0 +1,19 @@
+﻿namespace PDFService.API.Models
+{
+    public class Response
+    {
+        public Boolean error { get; set; }
+        public int status { get; set; }
+
+        public object? body { get; set; }
+
+        public Response(int status, bool error, object body)
+        {
+            this.status = status;
+            this.error = error;
+            this.body = body;
+
+        }
+
+    }
+}
